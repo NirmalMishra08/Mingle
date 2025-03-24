29f1ebb20ada0b26025972dd011c8f48e957e59e
NirmalMishra08 <145590866+NirmalMishra08@users.noreply.github.com>
2025-03-24 17:12:28 +0530

    Finished Backend

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(urlencoded({ extended: true }))
 const corsOptions = {
-    origin: 'https://frontend-gh58yhxmg-nirmal-mishras-projects.vercel.app',
+    origin: '*',
 
     
     credentials: true
