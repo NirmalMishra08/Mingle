35561946264f486a9982f711dd3a414876c9be9f
NirmalMishra08 <145590866+NirmalMishra08@users.noreply.github.com>
2025-03-25 21:53:55 +0530

    a

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(express.json())
 app.use(cookieParser())
 const corsOptions = {
     origin: 'https://mingle-lac.vercel.app', // ✅ exact frontend domain
-    credentials: true
+    credentials: false
   };
   
   app.use(cors(corsOptions));
